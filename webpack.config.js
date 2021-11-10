5d340d740b210e4af9dfb2253eb2937af246298e
GerardoBelard <belard86@gmail.com>
2021-11-10 17:03:17 -0600

    Webpack Funcionando

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-    //1. especificando el archivo de entrada 
-    entry: './client/index.js',
-    //2 especificar el archivo de salida 
+  //1. especificando el archivo de entrada
+  entry: "./client/index.js",
+  //2 especificar el archivo de salida
 
-    output: {
-        path: '/public' ,//3 la ruta absoluta de la salida
-        filename: 'bundle.js' //4 nombre del archivo de salida 
-    },
-    devServer: {
-        contentBase: './public'
-    }
-}
+  output: {
+    path: "/public", //3 la ruta absoluta de la salida
+    filename: "bundle.js", //4 nombre del archivo de salida
+  },
+  devServer: {
+    static: path.join(__dirname, "public"),
+    port: process.env.PORT || "3000",
+    host: "localhost",
+  },
+};
